d744e153b77c51b32db7a07e38e3e08688ec00e0
Nullreff <git@nullreff.net>
2023-07-12 20:33:29 -0700

    Switch to 2d

--- src/main.rs
+++ src/main.rs
@@ -1,80 +1,52 @@
-//! A shader that samples a texture with view-independent UV coordinates.
 
 use bevy::{
     prelude::*,
-    reflect::{TypePath, TypeUuid},
-    render::render_resource::{AsBindGroup, ShaderRef},
+    sprite::MaterialMesh2dBundle,
+    diagnostic::{FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin},
+    window::{CursorGrabMode, PresentMode, WindowLevel, WindowTheme},
+    app::PluginGroupBuilder,
 };
 
 fn main() {
     App::new()
-        .add_plugins((DefaultPlugins, MaterialPlugin::<CustomMaterial>::default()))
+        .add_plugins((
+            window_plugin(),
+            LogDiagnosticsPlugin::default(),
+            FrameTimeDiagnosticsPlugin,
+        ))
         .add_systems(Startup, setup)
-        .add_systems(Update, rotate_camera)
         .run();
+    
 }
 
-#[derive(Component)]
-struct MainCamera;
+fn window_plugin() -> PluginGroupBuilder {
+    DefaultPlugins.set(WindowPlugin {
+        primary_window: Some(Window {
+            title: "DMX Video".into(),
+            resolution: (500., 300.).into(),
+            present_mode: PresentMode::AutoVsync,
+            // Tells wasm to resize the window according to the available canvas
+            fit_canvas_to_parent: true,
+            // Tells wasm not to override default event handling, like F5, Ctrl+R etc.
+            prevent_default_event_handling: false,
+            window_theme: Some(WindowTheme::Dark),
+            ..default()
+        }),
+        ..default()
+    })
+}
 
 fn setup(
     mut commands: Commands,
-    asset_server: Res<AssetServer>,
     mut meshes: ResMut<Assets<Mesh>>,
-    mut custom_materials: ResMut<Assets<CustomMaterial>>,
-    mut standard_materials: ResMut<Assets<StandardMaterial>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
 ) {
-    commands.spawn(PbrBundle {
-        mesh: meshes.add(shape::Plane::from_size(5.0).into()),
-        material: standard_materials.add(Color::rgb(0.3, 0.5, 0.3).into()),
+    commands.spawn(Camera2dBundle::default());
+
+    commands.spawn(MaterialMesh2dBundle {
+        mesh: meshes.add(Mesh::from(shape::Quad::default())).into(),
+        transform: Transform::default().with_scale(Vec3::splat(128.)),
+        material: materials.add(ColorMaterial::from(Color::PURPLE)),
         ..default()
     });
-    commands.spawn(PointLightBundle {
-        transform: Transform::from_xyz(4.0, 8.0, 4.0),
-        ..default()
-    });
-
-    commands.spawn(MaterialMeshBundle {
-        mesh: meshes.add(Mesh::from(shape::Cube { size: 1.0 })),
-        transform: Transform::from_xyz(0.0, 0.5, 0.0),
-        material: custom_materials.add(CustomMaterial {
-            texture: asset_server.load(
-                "textures/FlightHelmet_Materials_LensesMat_OcclusionRoughMetal.png",
-            ),
-        }),
-        ..default()
-    });
-
-    // camera
-    commands.spawn((
-        Camera3dBundle {
-            transform: Transform::from_xyz(4.0, 2.5, 4.0).looking_at(Vec3::ZERO, Vec3::Y),
-            ..default()
-        },
-        MainCamera,
-    ));
-}
-
-fn rotate_camera(mut camera: Query<&mut Transform, With<MainCamera>>, time: Res<Time>) {
-    let cam_transform = camera.single_mut().into_inner();
-
-    cam_transform.rotate_around(
-        Vec3::ZERO,
-        Quat::from_axis_angle(Vec3::Y, 45f32.to_radians() * time.delta_seconds()),
-    );
-    cam_transform.look_at(Vec3::ZERO, Vec3::Y);
-}
-
-#[derive(AsBindGroup, Debug, Clone, TypeUuid, TypePath)]
-#[uuid = "b62bb455-a72c-4b56-87bb-81e0554e234f"]
-pub struct CustomMaterial {
-    #[texture(0)]
-    #[sampler(1)]
-    texture: Handle<Image>,
-}
-
-impl Material for CustomMaterial {
-    fn fragment_shader() -> ShaderRef {
-        "shaders/custom_material_screenspace_texture.wgsl".into()
-    }
 }
