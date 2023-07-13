be638a2606516be53b0dfac40a4b5337382afe24
Nullreff <git@nullreff.net>
2023-07-13 00:07:00 -0700

    Passing data to shader

---
+++ assets/shaders/multi_color_material.wgsl
@@ -0,0 +1,47 @@
+#import bevy_sprite::mesh2d_view_bindings   globals
+#import bevy_sprite::mesh2d_bindings        mesh
+#import bevy_sprite::mesh2d_functions       mesh2d_position_local_to_clip
+
+// TODO use common view binding
+#import bevy_render::view View
+
+@group(0) @binding(0)
+var<uniform> view: View;
+
+@group(1) @binding(0)
+var texture: texture_2d<f32>;
+@group(1) @binding(1)
+var texture_sampler: sampler;
+
+struct Vertex {
+    @location(0) position: vec3<f32>,
+};
+
+struct VertexOutput {
+    @builtin(position) clip_position: vec4<f32>,
+    @location(0) color: vec4<f32>,
+};
+
+@vertex
+fn vertex(vertex: Vertex) -> VertexOutput {
+    var out: VertexOutput;
+    out.clip_position = mesh2d_position_local_to_clip(mesh.model, vec4<f32>(vertex.position, 1.0));
+    out.color = vec4(1.0, 1.0, 1.0, 1.0);
+    return out;
+}
+
+struct FragmentInput {
+    @builtin(position) position: vec4<f32>,
+    @location(0) color: vec4<f32>,
+};
+
+@fragment
+fn fragment(input: FragmentInput) -> @location(0) vec4<f32> {
+    //let t = 0.05 * (0.85 + sin(5.0 * globals.time));
+    //return mix(vec4(1.0,1.0,1.0,1.0), vec4(1.0,1.0,1.0,1.0), smoothstep(t, t+0.01, 0));
+    let width = view.viewport[2];
+    let height = view.viewport[3];
+    let color = textureSample(texture, texture_sampler, vec2(0.0, 0.0));
+    return color;
+    //return vec4(input.position[0] / width, input.position[1] / height, 0.0, 1.0);
+}
--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,30 @@
 
+use std::iter;
 use bevy::{
     prelude::*,
-    sprite::MaterialMesh2dBundle,
+    sprite::{MaterialMesh2dBundle, Material2d, Material2dKey, Material2dPlugin},
     diagnostic::{FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin},
     window::{CursorGrabMode, PresentMode, WindowLevel, WindowTheme},
     app::PluginGroupBuilder,
+    reflect::{TypePath, TypeUuid},
+    render::{
+        render_resource::{AsBindGroup, ShaderRef, RenderPipelineDescriptor, SpecializedMeshPipelineError, Extent3d, TextureDimension, TextureFormat},
+        mesh::{MeshVertexBufferLayout, MeshVertexAttribute},
+    },
+    utils::Duration,
+    asset::ChangeWatcher,
 };
 
 fn main() {
     App::new()
         .add_plugins((
             window_plugin(),
+            Material2dPlugin::<MultiColorMaterial>::default(),
             LogDiagnosticsPlugin::default(),
             FrameTimeDiagnosticsPlugin,
         ))
         .add_systems(Startup, setup)
+        .add_systems(Update, test_system)
         .run();
     
 }
@@ -33,20 +43,110 @@ fn window_plugin() -> PluginGroupBuilder {
             ..default()
         }),
         ..default()
+    }).set(AssetPlugin {
+        watch_for_changes: ChangeWatcher::with_delay(Duration::from_millis(200)),
+        ..Default::default()
     })
+}
+
+fn generate_image(color: u8) -> Image {
+    Image::new(
+        Extent3d {
+            width: 48,
+            height: 1,
+            depth_or_array_layers: 1,
+        },
+        TextureDimension::D2,
+        iter::repeat(0).take(48)
+            .flat_map(|a| vec![color, color, color, 255])
+            .collect::<Vec<u8>>(),
+        TextureFormat::Rgba8UnormSrgb,
+    )
 }
 
 fn setup(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<ColorMaterial>>,
+    mut materials: ResMut<Assets<MultiColorMaterial>>,
+    mut images: ResMut<Assets<Image>>,
 ) {
     commands.spawn(Camera2dBundle::default());
 
-    commands.spawn(MaterialMesh2dBundle {
-        mesh: meshes.add(Mesh::from(shape::Quad::default())).into(),
-        transform: Transform::default().with_scale(Vec3::splat(128.)),
-        material: materials.add(ColorMaterial::from(Color::PURPLE)),
-        ..default()
-    });
+    let image = generate_image(255);
+    let handle = images.add(image);
+
+    commands.spawn(
+        MaterialMesh2dBundle {
+            mesh: meshes.add(Mesh::from(shape::Quad::default())).into(),
+            transform: Transform::default().with_scale(Vec3::splat(128.)),
+            material: materials.add(MultiColorMaterial {
+                texture: handle,
+            }),
+            ..default()
+        }
+    )
+    .insert(DmxGradient{});
 }
+
+/*
+fn dmx_system(time: Res<Time>, mut query: Query<(&mut DmxGradient, &mut MaterialMesh2dBundle<Image>)>, mut images: ResMut<Assets<Image>>,) {
+    for (mut dmx_gradient, mut bundle) in &mut query {
+        let color = (((time.elapsed_seconds() % 5.0) / 5.0) * 255.0) as u8;
+        let image = generate_image(color);
+        let texture = &bundle.material.texture;
+        texture = images.set(&texture, image);
+    }
+}
+*/
+
+
+fn test_system(
+    time: Res<Time>,
+    mut materials: ResMut<Assets<MultiColorMaterial>>,
+    mut images: ResMut<Assets<Image>>,
+    webview_query: Query<&Handle<MultiColorMaterial>, With<DmxGradient>>,
+) {
+    let material = materials.get_mut(webview_query.single()).unwrap();
+
+    let image = images.get_mut(&material.texture).unwrap();
+
+    let color = (((time.elapsed_seconds() % 5.0) / 5.0) * 255.0) as u8;
+    image
+        .data
+        .copy_from_slice(&[color, color, color, 255].repeat(48 * 1));
+}
+
+/// The Material trait is very configurable, but comes with sensible defaults for all methods.
+/// You only need to implement functions for features that need non-default behavior. See the Material api docs for details!
+impl Material2d for MultiColorMaterial {
+    fn fragment_shader() -> ShaderRef {
+        "shaders/multi_color_material.wgsl".into()
+    }
+    fn vertex_shader() -> ShaderRef {
+        "shaders/multi_color_material.wgsl".into()
+    }
+
+    fn specialize(
+        descriptor: &mut RenderPipelineDescriptor,
+        layout: &MeshVertexBufferLayout,
+        _key: Material2dKey<Self>,
+    ) -> Result<(), SpecializedMeshPipelineError> {
+        let vertex_layout = layout.get_layout(&[
+            Mesh::ATTRIBUTE_POSITION.at_shader_location(0),
+        ])?;
+        descriptor.vertex.buffers = vec![vertex_layout];
+        Ok(())
+    }
+}
+
+// This is the struct that will be passed to your shader
+#[derive(AsBindGroup, TypeUuid, TypePath, Debug, Clone)]
+#[uuid = "f690fdae-d598-45ab-8225-97e2a3f056e0"]
+pub struct MultiColorMaterial {
+    #[texture(0)]
+    #[sampler(1)]
+    texture: Handle<Image>,
+}
+
+#[derive(Component)]
+struct DmxGradient { }
