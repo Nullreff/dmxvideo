2a78a17f2460591d705124c75c5900504f76af60
Nullreff <git@nullreff.net>
2023-08-02 19:41:05 +0100

    Get sample config working

--- src/config.rs
+++ src/config.rs
@@ -17,6 +17,57 @@ pub const IMAGE_SIZE : usize = IMAGE_WIDTH * IMAGE_HEIGHT;
 pub const NETWORK_ADDRESS: &str = "0.0.0.0";
 pub const NETWORK_PORT: u16 = 6454;
 
+fn sample_config() -> Config {
+    Config {
+        network: Network {
+            address: "0.0.0.0".to_owned(),
+            port: 6454,
+        },
+        profiles: vec!(
+            Profile {
+                name: "Brightstripe".to_owned(),
+                channels: (0..48).into_iter().flat_map(|i| [Channel::Red(i), Channel::Green(i), Channel::Blue(i)]).collect(),
+            },
+        ),
+        mappings: vec!(
+            Mapping {
+                from: MappingGroup {
+                    profile: "Brigthstripe".to_owned(),
+                    patch: Patch {
+                        universe: 0,
+                        address: 1,
+                    }
+                },
+                to: MappingGroup {
+                    profile: "Brigthstripe".to_owned(),
+                    patch: Patch {
+                        universe: 0,
+                        address: 1,
+                    }
+                },
+                count: 28,
+            },
+        ),
+        outputs: vec!(
+            Output {
+                name: "Main".to_owned(),
+                width: 960,
+                height: 540,
+                fixtures: vec!(
+                    Fixture {
+                        position: Position { x: 0, y: 0 },
+                        size: Size { width: 100, height: 10 },
+                        grid: Grid { rows: 14, columns: 2 },
+                        mode: Mode::Rgb,
+                        layout: Layout { direction: Direction::Horizontal, count: 48 },
+                    }
+                ),
+            }
+        ),
+    }
+}
+
+
 #[derive(Serialize, Deserialize)]
 struct Network {
     address: String,
@@ -24,13 +75,31 @@ struct Network {
 }
 
 #[derive(Serialize, Deserialize)]
+enum Channel {
+    Dimmer(usize),
+    Red(usize),
+    Green(usize),
+    Blue(usize),
+    Shutter(usize),
+}
+
+#[derive(Serialize, Deserialize)]
 struct Profile {
     name: String,
+    channels: Vec<Channel>,
+}
+
+#[derive(Serialize, Deserialize)]
+struct MappingGroup {
+    profile: String,
+    patch: Patch,
 }
 
 #[derive(Serialize, Deserialize)]
 struct Mapping {
-
+    from: MappingGroup,
+    to: MappingGroup,
+    count: usize,
 }
 
 #[derive(Serialize, Deserialize)]
@@ -49,6 +118,17 @@ struct Size {
 struct Grid {
     rows: usize,
     columns: usize,
+}
+
+#[derive(Serialize, Deserialize)]
+enum Direction {
+    Horizontal,
+    Vertical,
+}
+
+#[derive(Serialize, Deserialize)]
+struct Layout {
+    direction: Direction,
     count: usize,
 }
 
@@ -62,7 +142,6 @@ struct Patch {
 enum Mode {
     Dimmer,
     Rgb,
-    Gradient,
 }
 
 #[derive(Serialize, Deserialize)]
@@ -71,7 +150,7 @@ struct Fixture {
     size: Size,
     grid: Grid,
     mode: Mode,
-
+    layout: Layout,
 }
 
 #[derive(Serialize, Deserialize)]
@@ -91,28 +170,13 @@ struct Config {
 }
 
 fn load_config(path: &str) -> Result<()> {
+    let input = std::fs::read_to_string(&path).unwrap();
 
     Ok(())
 }
 
-#[derive(Serialize, Deserialize)]
-struct Address {
-    street: String,
-    city: String,
-}
-
 pub fn save_config(path: &str) -> Result<()> {
-    // Some data structure.
-    let config = Config {
-        network: Network {
-            address: "0.0.0.0".to_owned(),
-            port: 6454,
-        },
-        profiles: vec!(),
-        mappings: vec!(),
-        outputs: vec!(),
-    };
-
+    let config = sample_config();
     // Serialize it to a JSON string.
     let j = serde_json::to_string(&config)?;
 
